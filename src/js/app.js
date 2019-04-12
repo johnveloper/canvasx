4667ce86fe7197d96fa9c91fcf500231d15c9cc4
johnveloper <johnveloper@gmail.com>
2019-04-12 16:34:28 +0200

    Add very simple AJAX content loading system

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,41 @@
 import Vue from 'vue/dist/vue.esm';
+import axios from 'axios';
 
 
 
-new Vue({
-	el: '#app'
+let content = [
+	{ path: 'intro', name: 'Introduction' },
+];
+
+
+
+Vue.component('sidebar-item', {
+	props: ['item', 'active'],
+	template: `
+		<a href="#" class="Sidebar-item"
+			:class="{ 'Sidebar-item--active': item.path == active }"
+			@click="$emit('click')"
+		>{{ item.name }}</a>
+	`
+});
+
+let app = new Vue({
+	el: '#app',
+	data: {
+		content,
+		active: 'intro',
+		contentHTML: '<h1>Loading...</h1>',
+	},
+	mounted() {
+		this.load(`${this.active}`);
+	},
+	methods: {
+		load(path) {
+			axios.get(`content/${path}.html`).then(res => this.contentHTML = res.data);
+		},
+		handleSidebarItemTap(item) {
+			this.active = item;
+			this.load(item);
+		}		
+	}
 });
